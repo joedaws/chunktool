14ad04c74f738c26159d65875668d52939b7f5c3
Joe <daws.joseph@gmail.com>
2024-01-24 10:03:10 -0500

    number of each thread appears before chunk

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,7 +10,7 @@ import System.IO
 threadify :: String -> IO ()
 threadify inputString = do
   let w = words inputString
-  let c = map unwords $ chunks w [] [] 280
+  let c = map unwords $ chunks w [] [] 1 280
   printThreads c
 
 printThreads :: [String] -> IO()
@@ -27,9 +27,9 @@ wordLengths str = map (\s -> (s, length s)) (words str)
 -- output is list of string chunks
 -- spaces count as characters so when checking for length we add in the
 -- number of spaces that will be in the final chunk
-chunks :: [String] -> [[String]] -> [String] -> Int -> [[String]]
-chunks [] acc [] _ = acc
-chunks [] acc currentChunk _ = acc ++ [currentChunk]
-chunks (x:xs) acc currentChunk limit
-  | length x + (sum $ map length currentChunk) + length currentChunk - 1 + length acc <= limit = chunks xs acc (currentChunk ++ [x]) limit
-  | otherwise = chunks (x:xs) (acc ++ [currentChunk]) [] limit
+chunks :: [String] -> [[String]] -> [String] -> Int -> Int -> [[String]]
+chunks [] acc [] _ _ = acc
+chunks [] acc currentChunk index _ = acc ++ [[show index ++ "-"] ++ currentChunk]
+chunks (x:xs) acc currentChunk index limit
+  | length x + (sum $ map length currentChunk) + length currentChunk - 1 + length acc + (length $ show index) + 2 <= limit = chunks xs acc (currentChunk ++ [x]) index limit
+  | otherwise = chunks (x:xs) (acc ++ [[show index ++ "-"] ++ currentChunk]) [] (index+1) limit
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,5 +7,5 @@ main = hspec $ do
     it "all chunks less than limit" $ do
       -- testing with input value of 10
       let limit = 20
-      let c = chunks (words "This is only a test of the functionality of string tool") [] [] limit
+      let c = chunks (words "This is only a test of the functionality of string tool") [] [] 1 limit
       all (<= limit) (map length $ map unwords c) `shouldBe` True

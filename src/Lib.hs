a2a547dd83b257bed86616421da9ef19553a5196
Joe <daws.joseph@gmail.com>
2024-01-26 00:07:03 -0500

    index indicator appears at end of chunks

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,8 +11,9 @@ import Control.Arrow (Arrow(second))
 -- Each component of a thread has at most 280 characters
 threadify :: String -> IO ()
 threadify inputString = do
-  let validWords = splitLongWords (words inputString) 280
-  let c = map unwords $ chunks validWords [] [] 1 280
+  let validWords = splitLongWords (words inputString) limit
+      c          = map unwords $ chunks validWords [] [] 1 limit
+      limit      = 280
   printThreads c
 
 printThreads :: [String] -> IO()
@@ -25,16 +26,20 @@ charCounts str = scanl (\(index, acc) y -> (index + 1, acc + length y)) (0,0) (w
 wordLengths :: String -> [(String, Int)]
 wordLengths str = map (\s -> (s, length s)) (words str)
 
+-- defines the separator between index and total
+separator :: [Char]
+separator = "/"
+
 -- input is list of words
 -- output is list of string chunks
 -- spaces count as characters so when checking for length we add in the
 -- number of spaces that will be in the final chunk
 chunks :: [String] -> [[String]] -> [String] -> Int -> Int -> [[String]]
 chunks [] acc [] _ _ = acc
-chunks [] acc currentChunk index _ = acc ++ [[show index ++ "-"] ++ currentChunk]
+chunks [] acc currentChunk index _ = acc ++ [currentChunk ++ [show index ++ separator]]
 chunks (x:xs) acc currentChunk index limit
   | length x + (sum $ map length currentChunk) + length currentChunk - 1 + length acc + (length $ show index) + 2 <= limit = chunks xs acc (currentChunk ++ [x]) index limit
-  | otherwise = chunks (x:xs) (acc ++ [[show index ++ "-"] ++ currentChunk]) [] (index+1) limit
+  | otherwise = chunks (x:xs) (acc ++ [currentChunk ++ [show index ++ separator]]) [] (index+1) limit
 
 -- words must have length less than the chunk limit otherwise
 -- chunks will fail
